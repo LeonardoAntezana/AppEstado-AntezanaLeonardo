52e8de13be6bc0094f11398957e9d2fcf759cfce
Leonardo <leonardoantezana59@gmail.com>
2023-03-10 12:35:30 -0300

    Cargando fuentes

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold,
+  })
+
+  useEffect(() => {
+    if(fontsLoaded){
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
