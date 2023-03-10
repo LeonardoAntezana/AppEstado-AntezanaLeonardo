0c45e36d5aa9f8568afa877f9d94988f76ffb91e
Leonardo <leonardoantezana59@gmail.com>
2023-03-10 13:19:38 -0300

    importando appNavigator

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import * as SplashScreen from 'expo-splash-screen'
+import AppNavigator from './src/navigator/AppNavigator';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -19,19 +20,14 @@ export default function App() {
   }, [fontsLoaded])
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <AppNavigator/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
