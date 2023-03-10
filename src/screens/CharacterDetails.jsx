00c49a3b5a43eb9884cc7a66d1f613f3c75db3d6
Leonardo <leonardoantezana59@gmail.com>
2023-03-10 12:43:21 -0300

    creando screens base

---
+++ src/screens/CharacterDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CharacterDetails = () => {
+  return (
+    <View>
+      <Text>CharacterDetails</Text>
+    </View>
+  )
+}
+
+export default CharacterDetails
+
+const styles = StyleSheet.create({})
---
+++ src/screens/ListCharacters.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ListCharacters = () => {
+  return (
+    <View>
+      <Text>ListCharacters</Text>
+    </View>
+  )
+}
+
+export default ListCharacters
+
+const styles = StyleSheet.create({})
