097efe3444cedd9809c694ed08d7dbea75d9c8c6
¨Leonardo <leonardoantezana59@gmail.com>
2023-03-15 10:19:11 -0300

    agregando componentes a la vista Home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { StyleSheet, View, Button} from 'react-native'
-import { COLORS } from '../constants/colors'
+import { FlatList, StyleSheet} from 'react-native'
+import { GridItem } from '../components';
+import {CATEGORIES} from '../../data/categories';
 
 const Home = ({ navigation }) => {
+  
+  const handleSelected = category => {
+    navigation.navigate('Categories', {
+      categoryId: category.id,
+      name: category.title
+    })
+  }
+
   return (
-    <View style={styles.screen}>
-      <Button title='Go to Categories' color={COLORS.primary} onPress={() => navigation.navigate('Categories')}/>
-    </View>
+    <FlatList
+    data={CATEGORIES}
+    renderItem={({item}) => <GridItem category={item} onSelected={handleSelected}/>}
+    keyExtractor={(category) => category.id}
+    numColumns={2}
+    />
   )
 }
 
