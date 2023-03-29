983f1dc9e4de5423a40dc5ee563ab3af9c4601db
Leonardo <leonardoantezana59@gmail.com>
2023-03-29 07:54:47 -0300

    guardando keys

---
+++ data/database.js
@@ -0,0 +1,3 @@
+export const URL_API = 'https://app-panaderia-96eb0-default-rtdb.firebaseio.com/'
+
+export const AUTH_API = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBVBXMVMUpq749RxEl3jWoRiaqHqr-r2U0'
